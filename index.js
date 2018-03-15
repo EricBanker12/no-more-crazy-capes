e323f554169e97d2fdb9f4ec749c3d19324a8d43
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2018-03-15 07:25:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // module for tera-proxy
 module.exports = function noMoreCrazyCapes(dispatch) {
     
+    const timeout = 3000
+    
     // variables
     let gameId = null,
-    timeouts = {}
+        timeouts = {}
     
     // find gameID on login
     dispatch.hook('S_LOGIN', 9, event => {
@@ -11,12 +13,12 @@ module.exports = function noMoreCrazyCapes(dispatch) {
     })
     
     // when someone is loaded
-    dispatch.hook('S_SPAWN_USER', 11, event => {
+    dispatch.hook('S_SPAWN_USER', 12, event => {
         check_appearance(event)
     })
     
     // when someone's equipment changes
-    dispatch.hook('S_USER_EXTERNAL_CHANGE', 4, event => {
+    dispatch.hook('S_USER_EXTERNAL_CHANGE', 5, event => {
        check_appearance(event)
     })
 
@@ -31,7 +33,7 @@ module.exports = function noMoreCrazyCapes(dispatch) {
             }
             // if using back costume, set timer to re-equip it
             if (event.styleBack != 0) {
-                timeouts[event.gameId] = setTimeout(refresh_appearance, 3000, event)
+                timeouts[event.gameId] = setTimeout(refresh_appearance, timeout, event)
             }
         }
     }
@@ -67,6 +69,37 @@ module.exports = function noMoreCrazyCapes(dispatch) {
             styleWeapon,
             styleBody,
             styleFootprint,
+            accessoryTransform1,
+            accessoryTransform2,
+            accessoryTransform3,
+            accessoryTransform4,
+            accessoryTransform5,
+            accessoryTransform6,
+            accessoryTransform7,
+            accessoryTransform8,
+            accessoryTransform9,
+            accessoryTransform10,
+            accessoryTransform11,
+            accessoryTransform12,
+            accessoryTransform13,
+            accessoryTransform14,
+            accessoryTransform15,
+            accessoryTransform16,
+            accessoryTransform17,
+            accessoryTransform18,
+            accessoryTransform19,
+            accessoryTransform20,
+            accessoryTransform21,
+            accessoryTransform22,
+            accessoryTransform23,
+            accessoryTransform24,
+            accessoryTransform25,
+            accessoryTransform26,
+            accessoryTransform27,
+            accessoryTransform28,
+            accessoryTransform29,
+            accessoryTransform30,
+            accessoryTransform31,
             styleBodyDye,
             showStyle
         } = event
